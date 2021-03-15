24d861e3265d83a1663d0d9602967ad360d2ab21
--global <eclaudio@gmail.com>
2021-03-15 15:14:54 -0400

    Close to aadding tweet content feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@
 #password:string virtual
 #password_confirmation:sting virtual
 class User < ApplicationRecord
+  has_many :twitter_accounts
+  has_many :tweets
   has_secure_password
 
   validates :email, presence: true, format: {with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/, message: "must be a vaild email" }
