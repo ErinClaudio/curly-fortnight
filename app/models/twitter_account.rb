898de9115f9c712410d041486b5d3cf5eed148b3
--global <eclaudio@gmail.com>
2021-03-17 11:02:02 -0400

    Added redis and sidekiq

--- app/models/twitter_account.rb
+++ app/models/twitter_account.rb
@@ -3,4 +3,14 @@ class TwitterAccount < ApplicationRecord
   has_many :tweets
 
   validates :username, uniqueness: true
+
+  def client
+    Twitter::REST::Client.new do |config|
+      config.consumer_key        =ENV['TWITTER_KEY']
+      config.consumer_secret     =ENV['TWITTER_SECRET']
+      config.access_token        = token
+      config.access_token_secret = secret
+    end
+  end
+
 end
